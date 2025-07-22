3f6ff824572605e7b5d664f82c858283fe734047
nopall .here <nauval.cool.28@gmail.com>
2025-07-22 08:47:49 +0700

    commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Jalankan seeder aplikasi.
+     */
+    public function run(): void
+    {
+        // Contoh pemanggilan seeder lain:
+        // $this->call(UserSeeder::class);
+    }
+}
